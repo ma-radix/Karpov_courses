86d14c562d7e54caeeade3b0e92d804025dc34c0
ma-radix <mari.korneeva.2001@yandex.ru>
2023-03-17 20:49:56 +0300

    lession 6 homework

--- SQL from Tabix/Lession 2/2.8_SQL.sql
+++
@@ -1,9 +0,0 @@
---Находим страны с наибольшей выручкой
-
-SELECT
-    Country,
-    SUM(UnitPrice * Quantity) AS Revenue -- количество проданного товара умножаем на его цену и сумируем по странам
-FROM retail
-GROUP BY Country
-ORDER BY Country DESC
-LIMIT 20
--- SQL from Tabix/Lession 4/4.4_SQL.sql
+++
@@ -1,9 +0,0 @@
---смотрим, в каком году и месяце зарегестрировалось больше всего хостов(кто сдаёт жильё)
-
-SELECT
-    COUNT(DISTINCT host_id) AS host_id_array,
-    toStartOfMonth(toDate32OrNull(host_since)) AS registration_date -- округляем к началу месяца, предворительно приведя к типу даты с возможностью получения Null
-FROM listings 
-GROUP BY registration_date
-ORDER BY host_id_array DESC
-LIMIT 20
